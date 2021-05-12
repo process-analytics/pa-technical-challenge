3783c1ae0d9ca7e2369f52467e93287be1432d7f
Celine Souchet <celine.souchet@bonitasoft.com>
2021-05-12 16:49:29 +0200

    Remove utils & move some const in the methods

--- library/ts-rollup/src/grid.ts
+++ library/ts-rollup/src/grid.ts
@@ -1,17 +1,12 @@
-import { getRandomInteger } from './utils';
 import { BingoGraph } from './graph';
 import { mxCell } from 'mxgraph';
 
-const title = 'BINGO';
 const titleHeight = 70;
 
 const columnWidth = 130;
 const rowHeight = 100;
 const columnNumber = 5;
 const rowNumber = 5;
-
-const width = columnWidth * columnNumber;
-const height = rowHeight * rowNumber + titleHeight;
 
 export class Grid {
   private readonly graph: BingoGraph;
@@ -23,6 +18,9 @@ export class Grid {
   resetGrid(): void {
     // Clear the whole grid for a full reset
     this.graph.getModel().clear();
+
+    const width = columnWidth * columnNumber;
+    const height = rowHeight * rowNumber + titleHeight;
 
     // Adds cells to the model in a single step
     this.graph.getModel().beginUpdate();
@@ -46,6 +44,8 @@ export class Grid {
   }
 
   private initializeTitle(grid: mxCell): void {
+    const title = 'BINGO';
+
     // display the title on first row
     for (let columnsCount = 0; columnsCount < columnNumber; columnsCount++) {
       const label = title.charAt(columnsCount);
@@ -78,4 +78,11 @@ export class Grid {
       }
     }
   }
+}
+
+// naive implementation, doesn't prevent duplicates in grid
+export function getRandomInteger(): number {
+  const min = 1;
+  const max = 90;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
