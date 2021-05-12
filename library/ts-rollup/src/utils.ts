2b83efd64dd63217ccbc6a21a3e6544d81fdbf9b
Celine Souchet <celine.souchet@bonitasoft.com>
2021-05-12 16:11:47 +0200

    Fix random numbers

--- library/ts-rollup/src/utils.ts
+++ library/ts-rollup/src/utils.ts
@@ -1,6 +1,6 @@
 // naive implementation, doesn't prevent duplicates in grid
 export function getRandomInteger(): number {
   const min = 1;
-  const max = 100;
+  const max = 90;
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
